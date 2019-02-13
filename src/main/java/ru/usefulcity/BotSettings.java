06a1a980a5e3d8225a37a08998106cebcef04725
stritron <finalroot@gmail.com>
2019-02-13 12:37:30 +0300

    travis

--- src/main/java/ru/usefulcity/BotSettings.java
+++ src/main/java/ru/usefulcity/BotSettings.java
@@ -1,5 +1,8 @@
 package ru.usefulcity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,6 +14,7 @@ public class BotSettings {
     public static final String PATH_TO_PROPERTIES = "resources/botconfig.properties";
     public static String BOT_USERNAME;
     public static String BOT_TOKEN;
+    private static Logger log = LoggerFactory.getLogger(BotSettings.class);
 
     static {
         try (FileInputStream fileInputStream = new FileInputStream(PATH_TO_PROPERTIES)) {
@@ -20,7 +24,7 @@ public class BotSettings {
             BOT_USERNAME = prop.getProperty("BOT_USERNAME");
             BOT_TOKEN = prop.getProperty("BOT_TOKEN");
         } catch (IOException e) {
-            e.printStackTrace();
+            log.info("Empty or no config file...");
         }
     }
 
