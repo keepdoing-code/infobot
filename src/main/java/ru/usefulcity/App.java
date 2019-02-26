20f3ac5f7593d696681a927d2bb9677ac64e8ddb
stritron <finalroot@gmail.com>
2019-02-26 19:37:10 +0300

    travis

--- src/main/java/ru/usefulcity/App.java
+++ src/main/java/ru/usefulcity/App.java
@@ -25,7 +25,6 @@ public class App {
             e.printStackTrace();
         }
 
-
         Log.out("Bot successfully started");
     }
 }
--- src/main/java/ru/usefulcity/BotSettings.java
+++ src/main/java/ru/usefulcity/BotSettings.java
@@ -3,6 +3,7 @@ package ru.usefulcity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -12,20 +13,31 @@ import java.util.Properties;
  */
 
 public class BotSettings {
-    public static final String PATH_TO_PROPERTIES = "botconfig.properties";
+    public static final String PROPERTIES_FILE = "botconfig.properties";
     public static String BOT_USERNAME;
     public static String BOT_TOKEN;
     private static Logger log = LoggerFactory.getLogger(BotSettings.class);
 
     static {
-        try (FileInputStream fileInputStream = new FileInputStream(PATH_TO_PROPERTIES)) {
+
+        String path = new File(BotSettings.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        File file = new File(path +" /" + PROPERTIES_FILE);
+
+        if (!file.exists()){
+            file = new File("./"+PROPERTIES_FILE);
+        }
+
+        log.info(file.getPath().toString());
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+
             Properties prop = new Properties();
             prop.load(fileInputStream);
 
             BOT_USERNAME = prop.getProperty("BOT_USERNAME");
             BOT_TOKEN = prop.getProperty("BOT_TOKEN");
         } catch (IOException e) {
-            log.info("Empty or no config file...");
+            log.error(path);
         }
     }
 
